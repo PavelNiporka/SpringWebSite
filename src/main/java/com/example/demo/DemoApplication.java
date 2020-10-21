9cc3d9ab5567cf21b1358d918ef5960aeafc701d
Niporka <pavelniporka@gmail.com>
2020-10-21 16:41:14 +0300

    WebSite

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-//
+
 //    @GetMapping("/hello")
 //    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
 //        return String.format("Hello %s!", name);
